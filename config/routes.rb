ad964b885da5f6365bf75decc47d63edbd8ac26d
Susanna Jacob <susanna@mgenlearn.com>
2019-11-15 11:51:39 +0000

    Re-Direct to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails do
+
+  root 'cocktails#index'
+
+  resources :cocktails, except: [:index] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
 end
-
 
 # , as: 'cocktails_id'
